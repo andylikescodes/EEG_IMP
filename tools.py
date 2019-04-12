9a358e88539f1a7853bdb869f1b8fc6ec69632b2
Andy Liang <andylikescodes@gmail.com>
2019-04-11 19:50:48 -0700

    able to generate images, but still need to go over the frequency plots

--- tools.py
+++ tools.py
@@ -25,6 +25,128 @@ SELECTED_CHANNELS = [13, 14, 15, 16, 17, 18, 19, 20,
  259, 260, 261, 262, 263, 264, 271, 272, 273, 274,
  275, 276, 277, 278, 279, 280, 281, 282]
 
+ELECTRODE_MAPPING_X = [ 1,  2,  2,  1,  2,  3,  3,  4,  4,  3,  4,  3,  4,  5,  5,  4,  5,
+        6,  6,  5,  5,  7,  6,  5,  6,  6,  6,  7,  8,  8,  7,  8,  7,  9,
+       10, 10,  9,  1,  2,  3,  2,  3,  4,  5,  4,  4,  3,  5,  6,  6,  5,
+        7,  8,  7,  6,  7,  8,  9, 10,  9,  8]
+
+ELECTRODE_MAPPING_Y = [4, 4, 5, 5, 3, 3, 4, 4, 5, 5, 1, 2, 2, 3, 4, 3, 5, 5, 6, 6, 1, 2,
+       1, 2, 2, 3, 4, 4, 4, 5, 5, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 6, 6, 7,
+       7, 8, 8, 8, 8, 9, 9, 6, 6, 7, 7, 8, 8, 6, 6, 7, 7]
+
+MAG_CHANNELS = [ 14,  17,  20,  23,  29,  32,  38,  41,  44,  47,  56,  59,  62,
+        65,  68,  71,  74,  77,  80,  83,  89,  98, 101, 104, 107, 110,
+       113, 116, 119, 122, 125, 134, 137, 140, 143, 146, 149, 176, 179,
+       182, 185, 200, 203, 206, 209, 212, 221, 224, 227, 230, 233, 248,
+       251, 254, 257, 260, 263, 272, 275, 278, 281]
+
+GRA1_CHANNELS = [ 12,  15,  18,  21,  27,  30,  36,  39,  42,  45,  54,  57,  60,
+        63,  66,  69,  72,  75,  78,  81,  87,  96,  99, 102, 105, 108,
+       111, 114, 117, 120, 123, 132, 135, 138, 141, 144, 147, 174, 177,
+       180, 183, 198, 201, 204, 207, 210, 219, 222, 225, 228, 231, 246,
+       249, 252, 255, 258, 261, 270, 273, 276, 279]
+
+GRAD2_CHANNELS = [ 13,  16,  19,  22,  28,  31,  37,  40,  43,  46,  55,  58,  61,
+        64,  67,  70,  73,  76,  79,  82,  88,  97, 100, 103, 106, 109,
+       112, 115, 118, 121, 124, 133, 136, 139, 142, 145, 148, 175, 178,
+       181, 184, 199, 202, 205, 208, 211, 220, 223, 226, 229, 232, 247,
+       250, 253, 256, 259, 262, 271, 274, 277, 280]
+
+# Single trial draw the frequency
+
+def freq_bands_power(signal):
+
+	current_signal_fft = abs(np.fft.fft(signal, n=500))
+	f = np.fft.fftfreq(500, d=1/500)
+
+	delta = sum(current_signal_fft[0:4])
+	theta = sum(current_signal_fft[4:8])
+	alpha = sum(current_signal_fft[8:14])
+	beta = sum(current_signal_fft[15:31])
+	low_gamma = sum(current_signal_fft[30:61])
+	high_gamma = sum(current_signal_fft[61:101])
+
+	return delta, theta, alpha, beta, low_gamma, high_gamma
+
+def freq_img(trial, window_mask, plot = True, figsize=(10, 14)):
+
+	delta_all_chs = []
+	theta_all_chs = []
+	alpha_all_chs = []
+	beta_all_chs = []
+	low_gamma_all_chs = []
+	high_gamma_all_chs = []
+
+	for i in range(len(trial)):
+		signal = trial[i, window_mask]
+		delta, theta, alpha, beta, low_gamma, high_gamma = freq_bands_power(signal)
+		delta_all_chs.append(delta)
+		theta_all_chs.append(theta)
+		alpha_all_chs.append(alpha)
+		beta_all_chs.append(beta)
+		low_gamma_all_chs.append(low_gamma)
+		high_gamma_all_chs.append(high_gamma)
+
+	all_bands = {'delta': delta_all_chs, 
+				 'theta': theta_all_chs, 
+				 'alpha': alpha_all_chs,
+				 'beta': beta_all_chs, 
+				 'low_gamma': low_gamma_all_chs, 
+				 'high_gamma': high_gamma_all_chs }
+
+	
+	imgs = []
+
+	for band in all_bands:
+		img = np.zeros((9,10))
+		for i in range(len(ELECTRODE_MAPPING_X)):
+			img[ELECTRODE_MAPPING_Y[i]-1, ELECTRODE_MAPPING_X[i]-1] = all_bands[band][i]
+
+		imgs.append(img)
+
+	imgs = np.stack(imgs)
+
+	if plot == True:
+		fig, ax = plt.subplots(nrows=3, ncols=2, figsize=figsize)
+		row_ind = 0
+		col_ind = 0
+
+		chs_names = list(all_bands.keys())
+		for i in range(len(imgs)):
+			ax[row_ind, col_ind].imshow(imgs[i])
+			ax[row_ind, col_ind].set_title(chs_names[i])
+			col_ind = col_ind + 1
+			if (col_ind == 2):
+				col_ind = col_ind - 2
+				row_ind = row_ind + 1
+
+	return imgs
+
+def plot_freq_bands(imgs, figsize=(10, 14)):
+	chs_names = ['delta', 'theta', 'alpha', 'beta', 'low_gamma', 'high_gamma']
+	fig, ax = plt.subplots(nrows=3, ncols=2, figsize=figsize)
+	row_ind = 0
+	col_ind = 0
+
+	for i in range(len(imgs)):
+		ax[row_ind, col_ind].imshow(imgs[i])
+		ax[row_ind, col_ind].set_title(chs_names[i])
+		col_ind = col_ind + 1
+		if (col_ind == 2):
+			col_ind = col_ind - 2
+			row_ind = row_ind + 1
+# Some util
+
+def load(filename):
+	cube = loadmat(filename)
+	data = cube['DATA_CUBE']
+	btn = cube['btn']
+	auto = cube['auto']
+	tAx = cube['tAx']
+	SR = cube['SR']
+	return data, btn, auto, tAx, SR
+
+# The electrode mapping tasks
 
 def process_channels():
 	filenames = glob.glob('./data/*.mat')
@@ -42,21 +164,6 @@ def process_channels():
 	gra1 = all_chs_selected.loc[all_chs_selected['type'] == 'gra1', :]
 	mag = all_chs_selected.loc[all_chs_selected['type'] == 'mag', :]
 	return gra2, gra1, mag
-
-
-# Tools
-
-def load(filepath):
-	"""
-	A handy program to extract several files of data for use.
-	"""
-	cube = loadmat(filepath)
-	data = cube['DATA_CUBE']
-	btn = cube['btn']
-	auto = cube['auto']
-	tAx = cube['tAx']
-	SR = cube['SR']
-	return data, btn, auto, tAx, SR
 
 def channel_mapping(df):
 	n = df.shape[0]
